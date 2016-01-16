82b08a787d0cf5fd99f1230d64807d7e4790240c
wood <liqingfengzzu@163.com>
2016-01-16 17:32:36 +0800

    完成图像灰度化样例

--- OpenCVDemo1/app/src/main/java/com/zzu/me/sample/MainActivity.java
+++ OpenCVDemo1/app/src/main/java/com/zzu/me/sample/MainActivity.java
@@ -1,13 +1,91 @@
 package com.zzu.me.sample;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageView mImgView;
+    private Button mButton;
+    private Bitmap mBitmap;
+    final static String TAG = "MainActivity";
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV Manager Connected");
+                    break;
+                case LoaderCallbackInterface.INIT_FAILED:
+                    Log.i(TAG,"Init Failed");
+                    break;
+                case LoaderCallbackInterface.INSTALL_CANCELED:
+                    Log.i(TAG,"Install Cancelled");
+                    break;
+                case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
+                    Log.i(TAG,"Incompatible Version");
+                    break;
+                case LoaderCallbackInterface.MARKET_ERROR:
+                    Log.i(TAG,"Market Error");
+                    break;
+                default:
+                    Log.i(TAG,"OpenCV Manager Install");
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //加载图像资源，并设置
+        mImgView = (ImageView)findViewById(R.id.imageView);
+        mBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.testimage1);
+        mImgView.setImageBitmap(mBitmap);
+        //添加Button Listener
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //将bitmap转换为Mat矩阵
+        Mat rgbMat = new Mat();
+        Utils.bitmapToMat(mBitmap, rgbMat);
+        //将彩色图像转换为灰度图像
+        Mat grayMat = new Mat();
+        Imgproc.cvtColor(rgbMat,grayMat,Imgproc.COLOR_RGB2GRAY);
+        //将Mat转换为bitmap并设置
+        Bitmap grayBmp = Bitmap.createBitmap(grayMat.width(),grayMat.height(), Bitmap.Config.RGB_565);
+        Utils.matToBitmap(grayMat,grayBmp);
+        mImgView.setImageBitmap(grayBmp);
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # OpenCV Demo for Android Studio
 展示使用Android Studio 完成第一个OpenCV样例程序  
 
-1. 样例1：OpenCVDemo1 利用OpenCV Manager程序并直接使用java接口，完成图像的灰度化
+1. 样例1：OpenCVDemo1 直接使用java接口，完成图像的灰度化
